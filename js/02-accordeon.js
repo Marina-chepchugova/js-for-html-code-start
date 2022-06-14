8176113abb0d79c29c7cbe2c96ab4a5ab57bb10f
MarinaChepchugova <chepchugova.m82@gmail.com>
2022-06-14 23:05:39 +1100

    js

---
+++ js/02-accordeon.js
@@ -0,0 +1,10 @@
+const headers = document.querySelectorAll('[data-name = "accordeon-title"]');
+
+headers.forEach(function(item){
+	item.addEventListener('click' , а)
+	
+})
+function а ()
+{console.log(this)
+
+}
